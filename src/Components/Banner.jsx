fe5b4b04a43ddd9656987cc239fed8f2e8ed828f
Shoaib Sharif <shoaibsharif85@gmail.com>
2023-10-23 08:22:18 -0700

    git hub

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,13 +1,13 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import Typed from 'react-typed';
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
 const Banner = () => {
-    
-    useEffect(()=>{
-        Aos.init({duration: 2000})
-    },[])
+
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+    }, [])
 
     return (
         <div className='bg-[#2699fb] w-full py-[90px]'>
@@ -18,11 +18,11 @@ const Banner = () => {
                 <h2 data-aos="fade-left" className='text-white font-bold  text-3xl md:text-[80px] pt-4 mt-7'>Shoaib Sharif</h2>
                 <div data-aos="fade-right" className='text-[30px]  md:text-[50px] text-white font-bold pl-3 mt-7'>
                     I'm  <Typed
-                    strings={[ 'React developer','Mern Stack developer', 'Front-end develper']}
-                    typeSpeed={100}
-                    loop={true}
-                    backSpeed={50}
-                />
+                        strings={['React developer', 'Mern Stack developer', 'Front-end develper']}
+                        typeSpeed={100}
+                        loop={true}
+                        backSpeed={50}
+                    />
                 </div>
                 <button data-aos="fade-left" className='bg-[black] text-white p-[12px] md:p-[17px] w-[110px] md:w-[150px] mt-8  rounded '>Get Started</button>
             </div>
